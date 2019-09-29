6ebb980b05723b57a1ae604188cb2bd0c14e500c
pazthiago1 <pazthiagolima@gmail.com>
2019-09-29 03:04:29 -0300

    Implementa comunicação com a API de cadastro de usuario

--- app/src/main/java/br/com/youthquake/MainActivity.kt
+++ app/src/main/java/br/com/youthquake/MainActivity.kt
@@ -1,12 +1,31 @@
 package br.com.youthquake
 
+import android.app.Service
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.youthquake.config.ConfigRetrofit
+import br.com.youthquake.data.User
+import br.com.youthquake.service.UserService
+import kotlinx.android.synthetic.main.sign_in.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btCadastrar.setOnClickListener {
+            val usr = User()
+            usr.name = etNome.text.toString()
+            usr.login = etUsername.text.toString()
+            usr.password = etSenha.text.toString()
+            usr.email = etEmail.text.toString()
+            val call = ConfigRetrofit().userInclude().insertUser(usr)
+            call.execute()
+        }
     }
 }
--- app/src/main/java/br/com/youthquake/config/ConfigRetrofit.kt
+++ app/src/main/java/br/com/youthquake/config/ConfigRetrofit.kt
@@ -1,14 +1,20 @@
 package br.com.youthquake.config
 
+import br.com.youthquake.data.User
+import br.com.youthquake.service.UserService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ConfigRetrofit {
 
-    fun init(){
+    private val retrofit =
         Retrofit.Builder()
-            .baseUrl("http://192.168.0.1:8080/")
+            .baseUrl("http://192.168.0.104:8080/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+
+    fun userInclude():UserService{
+        return retrofit.create(UserService::class.java)
     }
 }
